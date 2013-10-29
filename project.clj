27b8c043d1083a37c1de1569a7afd394df453f56
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-10-29 08:11:10 -0700

    CORE-4922: Added openjdk as a dependency.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [slingshot "0.10.3"]]
   :plugins [[org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"]]
   :iplant-rpm {:summary "osm"
-               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin"]
+               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin" "java-1.7.0-openjdk"]
                :config-files ["log4j.properties"]
                :config-path "conf"}
   :aot [osm.core]
